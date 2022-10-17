495669e1c70eb54ea2edebcead559dd6c04bef9b
rich26638 <ishimwerichard26@gmail.com>
2022-10-17 22:34:56 +0200

    deleted file

--- test_package/test2.java
+++
@@ -1,9 +0,0 @@
-
-package test_package;
-public class test2 {
-    int x= 5;
-    public static void main(String []args){
-        test object = new test();
-        System.out.println(object.x);
-    }
-}
